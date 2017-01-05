538109a3fadbe0d5454fbfd6d4dbf271a1b0e721
Lyndon Michael Bibera <lyndon.bibera@boltoninternational.com>
2017-01-05 16:47:12 +0800

    initial commit

---
+++ src/main/java/com/hadooppractice/App.java
@@ -0,0 +1,35 @@
+package com.hadooppractice;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import com.hadooppractice.wordcount.TextCountMapper;
+import com.hadooppractice.wordcount.TextCountReducer;
+
+public class App {
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+		Configuration conf = new Configuration();
+
+		Job job = Job.getInstance(conf, "word-count");
+
+		job.setJarByClass(App.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(LongWritable.class);
+
+		job.setMapperClass(TextCountMapper.class);
+		job.setReducerClass(TextCountReducer.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		job.waitForCompletion(true);
+	}
+}
---
+++ src/main/java/com/hadooppractice/wordcount/TextCountMapper.java
@@ -0,0 +1,22 @@
+package com.hadooppractice.wordcount;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class TextCountMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+	private final static LongWritable one = new LongWritable(1);
+	private Text word = new Text();
+
+	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+		String line = value.toString();
+		StringTokenizer tokenizer = new StringTokenizer(line);
+		while (tokenizer.hasMoreTokens()) {
+			word.set(tokenizer.nextToken());
+			context.write(word, one);
+		}
+	}
+}
