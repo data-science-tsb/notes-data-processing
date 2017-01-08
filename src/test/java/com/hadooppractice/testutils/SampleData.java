ae2fe64b7e570305a5895a20a3717e53fb86f830
Lyndon Michael Bibera <hack.of.all.codes@gmail.com>
2017-01-08 14:52:06 +0800

    added more sample data

--- src/test/java/com/hadooppractice/testutils/SampleData.java
+++ src/test/java/com/hadooppractice/testutils/SampleData.java
@@ -1,4 +1,4 @@
-package com.hadooppractice.util;
+package com.hadooppractice.testutils;
 
 /**
  * Created by lbibera on 08/01/2017.
@@ -7,15 +7,21 @@ public class SampleData {
 
     public static final String COMMENT_ROW = "  <row Id=\"2579740\" PostId=\"2573882\" Text=\"Are you getting any results? What\n" +
             "are you specifying as the command text?\" CreationDate=\"2010-04-04T08:48:51.347\" UserId=\"95437\" />";
+    public static final String COMMENT_ROW_USER_ID = "95437";
+    public static final String COMMENT_ROW_CREATION_DATE = "2010-04-04T08:48:51.347";
 
-    public static final String POST_ROW_1 = "  <row Id=\"6939296\" PostTypeId=\"2\" ParentId=\"6939137\" CreationDate=\"2011-08-04T09:50:25.043\" " +
+    public static final String POST_QUESTION_ROW = "  <row Id=\"6939296\" PostTypeId=\"2\" ParentId=\"6939137\" CreationDate=\"2011-08-04T09:50:25.043\" " +
             "Score=\"4\" ViewCount=\"\" Body=\"&lt;p&gt;You should have imported Poll with &lt;code&gt; from polls.models import Poll&lt;/code&gt;&lt;/p&gt;&#xA;\" " +
             "OwnerUserId=\"634150\" LastActivityDate=\"2011-08-04T09:50:25.043\" CommentCount=\"1\" />\n";
 
-    public static  final String POST_ROW_2 = "  <row Id=\"6939304\" PostTypeId=\"1\" AcceptedAnswerId=\"6939433\" CreationDate=\"2011-08-04T09:50:58.910\" Score=\"1\" " +
+    public static final String POST_ANSWER_ROW = "  <row Id=\"6939304\" PostTypeId=\"1\" AcceptedAnswerId=\"6939433\" CreationDate=\"2011-08-04T09:50:58.910\" Score=\"1\" " +
             "ViewCount=\"26\" Body=\"&lt;p&gt;Is it possible to gzip a single asp.net 3.5 page? my\n" +
             "site is hosted on IIS7 and for technical reasons I cannot enable gzip compression site wide. " +
             "does IIS7 have an option to gzip individual pages or will I have to override OnPreRender and write some code to compress the output?&lt;/p&gt;&#xA;\" " +
             "OwnerUserId=\"743184\" LastActivityDate=\"2011-08-04T10:19:04.107\" Title=\"gzip a single asp.net page\" Tags=\"&lt;asp.net&gt;&lt;iis7&gt;&lt;gzip&gt;\"\n" +
             "AnswerCount=\"2\" />";
+
+    public static final String USER = "  <row Id=\"352268\" Reputation=\"3313\" CreationDate=\"2010-05-27T18:34:45.817\" " +
+            "isplayName=\"orangeoctopus\" EmailHash=\"93fc5e3d9451bcd3fdb552423ceb52cd\" LastAccessDate=\"2011-09-01T13:55:02.013\" " +
+            "Location=\"Maryland\" Age=\"26\" Views=\"48\" UpVotes=\"294\" DownVotes=\"4\" />";
 }
