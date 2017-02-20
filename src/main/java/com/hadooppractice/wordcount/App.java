c6f09c3d8ed0a6d9cb29f9ee58018958e8bfb9a8
Lyndon Michael Bibera <lyndon.bibera@boltoninternational.com>
2017-02-20 12:48:16 +0800

    modernized wordcount, tested this on live cluster

--- src/main/java/com/hadooppractice/wordcount/App.java
+++ src/main/java/com/hadooppractice/wordcount/App.java
@@ -1,32 +1,39 @@
 package com.hadooppractice.wordcount;
 
-import java.io.IOException;
-
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
-public class App {
+public class App extends Configured implements Tool {
 
-	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
-		Configuration conf = new Configuration();
-
-		Job job = Job.getInstance(conf, "word-count");
-
-		job.setJarByClass(App.class);
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(LongWritable.class);
-
-		job.setMapperClass(TextCountMapper.class);
-		job.setReducerClass(TextCountReducer.class);
-
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-
-		job.waitForCompletion(true);
+	public static void main(String[] args) throws Exception {
+        int res = ToolRunner.run(new Configuration(), new App(), args);
+        System.exit(res);
 	}
+
+    @Override
+    public int run(String[] args) throws Exception {
+        Configuration conf = getConf();
+
+        Job job = Job.getInstance(conf, "word-count");
+
+        job.setJarByClass(App.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+
+        job.setMapperClass(TextCountMapper.class);
+        job.setReducerClass(TextCountReducer.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
 }
