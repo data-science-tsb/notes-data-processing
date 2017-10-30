41202bbc3910db6b7852d3cec52e0718576fa7f5
LM Bibera <secret.transaction@gmail.com>
2017-10-31 00:08:14 +0800

    implemented assignment: aggregate customer order amount

---
+++ Spark/project-udemy-spark/src/main/scala/com/psicoder/spark/udemy/CustomerSpending.scala
@@ -0,0 +1,30 @@
+package com.psicoder.spark.udemy
+
+import com.psicoder.spark.udemy.util.{ContextLoader, FileLoader}
+
+object CustomerSpending {
+
+  case class CustomerOrder(customerId: Int, orderId: Int, amount: Float)
+
+  def main(args: Array[String]): Unit = {
+    val sc = ContextLoader.resolveSparkContext(args, "CustomerSpending")
+    val file = sc.textFile(FileLoader.resolveFile(args, "customer-orders.csv"))
+
+    val orderSummary = file.map { text =>
+        val customerOrder = parse(text)
+        (customerOrder.customerId, customerOrder.amount)
+      }
+      .reduceByKey(_+_)
+      .map { case(k, v) => (v, k) }
+      .sortByKey()
+
+    orderSummary
+      .collect()
+      .foreach { case(k, v) => println(s"$v : $k")}
+  }
+
+  def parse(text: String): CustomerOrder = {
+    val fields = text.split(",")
+    CustomerOrder(fields(0).toInt, fields(1).toInt, fields(2).toFloat)
+  }
+}
