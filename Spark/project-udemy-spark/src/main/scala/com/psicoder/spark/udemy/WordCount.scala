c991d2850a80760493c2466a868e0db713c1943c
LM Bibera <secret.transaction@gmail.com>
2017-10-30 23:34:12 +0800

    improved word count by adding sorting using RDD

--- Spark/project-udemy-spark/src/main/scala/com/psicoder/spark/udemy/WordCount.scala
+++ Spark/project-udemy-spark/src/main/scala/com/psicoder/spark/udemy/WordCount.scala
@@ -2,17 +2,26 @@ package com.psicoder.spark.udemy
 
 import com.psicoder.spark.udemy.util.{ContextLoader, FileLoader}
 
+import Math.addExact
+
 object WordCount {
 
   def main(args: Array[String]): Unit = {
     val sc = ContextLoader.resolveSparkContext(args, "WordCount")
     val file = sc.textFile(FileLoader.resolveFile(args, "Book.txt"))
 
-    val wordCounts = file.flatMap(normalizeWords).countByValue()
+    val wordCounts = file
+      .flatMap(normalizeWords)
+      .map((_, 1))
+      .reduceByKey(addExact)
 
-    wordCounts.toSeq.sortBy(_._2).foreach {
-      case (word, count) => println(s"$word : $count")
-    }
+    val wordCountsSorted = wordCounts
+      .map { case(k, v) => (v, k) }
+      .sortByKey()
+
+    wordCountsSorted
+      .collect()
+      .foreach { case (count, word) => println(s"$word : $count") }
   }
 
   def normalizeWords(text: String) = """\W""".r.split(text.toLowerCase)
