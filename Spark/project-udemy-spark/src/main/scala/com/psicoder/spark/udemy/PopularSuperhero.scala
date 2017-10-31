76cc3b0d79fdf1abeb6fc6cf922824f6e9c47de6
LM Bibera <secret.transaction@gmail.com>
2017-10-31 16:32:02 +0800

    popular superheroes: implemented

--- Spark/project-udemy-spark/src/main/scala/com/psicoder/spark/udemy/PopularSuperhero.scala
+++ Spark/project-udemy-spark/src/main/scala/com/psicoder/spark/udemy/PopularSuperhero.scala
@@ -9,10 +9,31 @@ object PopularSuperhero {
     val graphFile = sc.textFile(FileLoader.resolveFile(args, "Marvel-Graph.txt"))
     val namesFile = sc.textFile(FileLoader.resolveFile(args, "Marvel-Names.txt"))
 
-    println("Sample Graph:")
-    graphFile.take(10).foreach(println)
+    val namesRDD = namesFile.map(parseNames)
 
-    println("Sample Names:")
-    namesFile.take(10).foreach(println)
+    val heroCoccurence = graphFile
+      .map(countCoOccurences)
+      .reduceByKey(_+_)
+      .map(_.swap)
+      .sortByKey(false)
+
+    val mostPopular = heroCoccurence.first()
+    val mostPopularName = namesRDD.lookup(mostPopular._2)(0)
+
+    println(s"The most popular superhero is $mostPopularName with ${mostPopular._1} co-appearances")
+  }
+
+  def countCoOccurences(line: String): (Int, Int) = {
+    val fields = line.split(" ")
+    (fields(0).toInt, fields.length - 1)
+  }
+
+  def parseNames(line: String): (Int, String) = {
+    val fields = line.split("\"")
+
+    val id = fields(0).trim().toInt
+    val name = if (fields.isDefinedAt(1)) fields(1) else  "Unknown"
+
+    (id, name)
   }
 }
