62ccf9348e73007bc0c19b7cd657bd2a2e10e129
LM Bibera <secret.transaction@gmail.com>
2017-11-01 12:57:59 +0800

    degrees of separation: rough draft of the breadth-first search algorithm

--- Spark/project-udemy-spark/src/main/scala/com/psicoder/spark/udemy/DegreesOfSeparation.scala
+++ Spark/project-udemy-spark/src/main/scala/com/psicoder/spark/udemy/DegreesOfSeparation.scala
@@ -1,10 +1,16 @@
 package com.psicoder.spark.udemy
 
 import com.psicoder.spark.udemy.util.{ContextLoader, FileLoader}
+import org.apache.spark.util.LongAccumulator
 
+/**
+  * calculate the degree of separation between two nodes
+  */
 object DegreesOfSeparation {
 
   val MAX_DISTANCE = 9999
+  val startHeroId = 5306 //SpiderMan
+  val targetHeroId = 14 //Adam
 
   object VisitStatus extends Enumeration {
     val NotVisited, Visiting, Visited = Value
@@ -16,14 +22,45 @@ object DegreesOfSeparation {
     val sc = ContextLoader.resolveSparkContext(args, "PopularSuperhero")
     val graphFile = sc.textFile(FileLoader.resolveFile(args, "Marvel-Graph.txt"))
 
+    val hitCounter = sc.longAccumulator("hitCounter")
 
-    val nodes = graphFile.map(parse)
-    nodes.take(10).foreach(println)
+    var iterationRDD = graphFile.map(parse)
+    for (iteration <- 0 to 10) {
+
+      val mapped = iterationRDD.flatMap(expandNode(_, hitCounter))
+
+      //cheat code to force flatMap evaluation
+      iterationRDD.count()
+
+      if (!hitCounter.isZero) {
+        //TODO: break off this loop
+      }
+
+      iterationRDD = mapped.reduceByKey(reduce)
+
+    }
   }
 
-  def parse(line: String): HeroNode = {
+  def parse(line: String): (Int, HeroNode) = {
     val fields = line.split(" ")
-    HeroNode(fields(0).toInt, fields.drop(1).map(_.toInt), MAX_DISTANCE, VisitStatus.NotVisited)
+
+    val heroId = fields(0).toInt
+
+    val (visitStatus, distance) =
+      if (heroId == startHeroId) (VisitStatus.Visiting, 0)
+      else (VisitStatus.NotVisited, MAX_DISTANCE)
+
+    (heroId, HeroNode(heroId, fields.drop(1).map(_.toInt), distance, visitStatus))
+  }
+
+  def expandNode(currentNode: (Int, HeroNode), hitCounter: LongAccumulator): Seq[(Int, HeroNode)] = {
+    //TODO: implement
+    Array(currentNode)
+  }
+
+  def reduce(a: HeroNode, b: HeroNode): HeroNode = {
+    //TODO: implement
+    HeroNode(1,Array(1,2,3),MAX_DISTANCE,VisitStatus.Visited)
   }
 
 }
