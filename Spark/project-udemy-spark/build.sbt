5bd8fb297dd094749f85aa20a3da24666ee1c002
Lyndon Michael Bibera <lyndon.bibera@boltoninternational.com>
2017-11-21 17:18:56 +0800

    degrees of separation: improved unit test and fixed error

--- Spark/project-udemy-spark/build.sbt
+++ Spark/project-udemy-spark/build.sbt
@@ -6,6 +6,10 @@ scalaVersion := "2.11.4"
 
 val sparkVersion = "2.1.0"
 
+//conflict
+libraryDependencies += "commons-net" % "commons-net" % "3.6"
+libraryDependencies += "com.google.guava" % "guava" % "14.0.1"
+
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % sparkVersion,
   "org.apache.spark" %% "spark-sql" % sparkVersion,
@@ -14,4 +18,5 @@ libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-hive" % sparkVersion
 )
 
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.4"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.4" % "test"
--- Spark/project-udemy-spark/src/test/scala/com/psicoder/spark/udemy/DegreesOfSeparationSpec.scala
+++ Spark/project-udemy-spark/src/test/scala/com/psicoder/spark/udemy/DegreesOfSeparationSpec.scala
@@ -5,34 +5,34 @@ import com.psicoder.spark.udemy.test.UnitSpec
 
 class DegreesOfSeparationSpec extends UnitSpec {
 
-  "Reduce" should "choose the Visited Status when one of the parameter is visited" in {
+  "DegreesOfSeparation.reduce(heroNodeA, heroNodeB)" should "choose the Visited Status when one of the parameter is visited" in {
     val nodeA = HeroNode(Array(), 0, VisitStatus.Visited)
     val nodeB = HeroNode(Array(), 0, VisitStatus.NotVisited)
 
     val reducedNode = DegreesOfSeparation.reduce(nodeA, nodeB)
 
-    assert(reducedNode.visitStatus == VisitStatus.Visited)
+    assert(reducedNode.visitStatus === VisitStatus.Visited)
   }
 
-  "Reduce" should "choose the Visiting Status when one of the parameter is visiting" in {
+  it should "choose the Visiting Status when one of the parameter is visiting" in {
     val nodeA = HeroNode(Array(), 0, VisitStatus.Visiting)
     val nodeB = HeroNode(Array(), 0, VisitStatus.NotVisited)
 
     val reducedNode = DegreesOfSeparation.reduce(nodeA, nodeB)
 
-    assert(reducedNode.visitStatus == VisitStatus.Visiting)
+    assert(reducedNode.visitStatus === VisitStatus.Visiting)
   }
 
-  "Reduce" should "pick the shorter distance" in {
+  it should "pick the shorter distance" in {
     val nodeA = HeroNode(Array(), 1, VisitStatus.Visiting)
     val nodeB = HeroNode(Array(), 2, VisitStatus.Visiting)
 
     val reducedNode = DegreesOfSeparation.reduce(nodeA, nodeB)
 
-    assert(reducedNode.distance == 1)
+    assert(reducedNode.distance === 1)
   }
 
-  "Reduce" should "combine the connections distinctly" in {
+  it should "combine the connections distinctly" in {
     val nodeA = HeroNode(Array(1, 2, 3, 4, 5), 1, VisitStatus.Visiting)
     val nodeB = HeroNode(Array(3, 4, 5, 6, 7, 8), 2, VisitStatus.Visiting)
 
@@ -40,11 +40,11 @@ class DegreesOfSeparationSpec extends UnitSpec {
 
     val connections = reducedNode.connections
 
-    assert(connections.length == 8)
+    assert(connections.length === 8)
 
-    assert(connections(0) == 1)
-    assert(connections(4) == 5)
-    assert(connections(5) == 6)
-    assert(connections(7) == 8)
+    assert(connections(0) === 1)
+    assert(connections(4) === 5)
+    assert(connections(5) === 6)
+    assert(connections(7) === 8)
   }
 }
